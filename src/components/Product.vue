414a4e6d6de8c1c1c800fdedfbb010f01c6fed07
Meirunas Smitas <smeirunas@email.com>
2023-11-26 22:42:00 +0000

    fix: add file

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -210,90 +210,39 @@ import '../style.css'
 
 
   <fwb-modal v-if="isShowModal" @close="closeModal" size="5xl">
-    <template #header>
-      <div class="flex justify-center">
-        <h1 class="font-bold" id="vidh" style="color: black; cursor: pointer;" @click="isVideoModal">Video</h1>
-        <h1 class="font-bold pl-10" id="imgh" style="color: gray;  cursor: pointer;" @click="isImageModa">Images
-        </h1>
-      </div>
-    </template>
-    <template #body>
-      <div class="flex" style="background: white;">
-
-        <div class="basis-[61%]">
-
-          <div class="flex px-3 py-8 bg-white" v-if="isShowModalVideo">
-
-            <div class="zoom-preview1">
-
+    <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
             </div>
-            <div class="relative overflow-hidden">
-              <div class="flex transition ease-in-out myImg" :style="{
-                transform: `translateX(-${(currentSlideModal - 1) * 100}%)`,
-              }">
-                <div v-for="(slide, index) in slides" :key="index" class="flex-none w-full flex justify-center">
-                  <template v-if="slide.type === 'image'">
-                    <img :src="slide.main" :id="index" class="object-contain" @mouseover="showZoom1(index)"
-                      @mousemove="showZoom1(index)" style="width: 35rem;height: 35rem;" />
-                  </template>
-
-                </div>
-              </div>
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
             </div>
-          </div>
-
-          <div class="flex px-3 py-8 bg-white" v-else="isShowModalVideo">
-
-
-            <div class="relative overflow-hidden">
-              <div class="flex transition ease-in-out myImg" :style="{
-                transform: `translateX(-${(currentSlideModal - 1) * 100}%)`,
-              }">
-                <div v-for="(slide, index) in slides" :key="index" class="flex-none w-full flex justify-center">
-
-                  <template v-if="slide.type === 'video'">
-                    <video :src="slide.src" controls muted autoplay loop alt="Product Video"
-                      style="width: 75rem;height: 35rem;" />
-                  </template>
-                </div>
-              </div>
+            <!-- Modal footer -->
+            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="default-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
             </div>
-          </div>
-
-
-
         </div>
-
-        <div class="flex basis-[23%] flex-col py-6 pr-4 pl-28">
-          <div class="flex justify-between">
-            <h2 class="text-slate-400" style="color: black;">SAMSUMNG</h2>
-
-          </div>
-          <div class="flex flex-col">
-            <h1 class="font-bold" style="color: black;">Samsung galaxy S23 Plus 5G</h1>
-          </div>
-          <div class="flex justify-center align-items-center pb-6 " v-if='isShowModalVideo'>
-            <template v-for="(slide, index) in slides" :key="index">
-              <img v-if="slide.type === 'image'" :src="slide.thumbnail" :alt="slide.alt"
-                @click="goToSlideModal(index + 1)" :class="{ thumbnailsActive: index + 1 === currentSlideModal }"
-                class="mx-4 w-16 p-2 tile" />
-
-
-            </template>
-          </div>
-
-          <div class="flex justify-center align-items-center pb-6" v-else='isShowModalVideo'>
-            <template v-for="(slide, index) in slides" :key="index">
-
-              <div v-if="slide.type === 'video'" class="flex items-center mx-3 w-16" @click="goToSlideModal(index + 1)"
-                :class="{ thumbnailsActive: index + 1 === currentSlideModal }">
-                <img src="../assets/icon-video.svg" alt="" />
-              </div>
-            </template>
-          </div>
-        </div>
-      </div>
-    </template>
+    </div>
+</div>
   </fwb-modal>
 </template>
 
